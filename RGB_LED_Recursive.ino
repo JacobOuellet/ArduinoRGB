da7b4d1e0453c844ad403a166fa4954b9832619f
JacobOuellet <39535221+JacobOuellet@users.noreply.github.com>
2018-05-23 10:10:57 -0400

    Update RGB_LED_Recursive.ino

--- RGB_LED_Recursive.ino
+++ RGB_LED_Recursive.ino
@@ -2,15 +2,15 @@
 //2016.12.8
 
 // Define Pins and rgb values
-int redValue = 255;
+int redValue = 255; 
 int greenValue = 0;
 int blueValue = 0;
-static int currentPin = 6;
-static int nextPin = 5;
+static int currentPin = 6; // these pins represent the pwm pins on the arduino where you connect the led red, green and blue to. 
+static int nextPin = 5;     // e.g. red lead is conneceted to pin 6, green lead connected to 5, blue lead is connected to pin 3
 static int inWaitingPin = 3;
 void setup()
 {
-pinMode(currentPin, OUTPUT);
+pinMode(currentPin, OUTPUT); // initializing pins
 pinMode(nextPin, OUTPUT);
 pinMode(inWaitingPin, OUTPUT);
 digitalWrite(currentPin, HIGH);
@@ -20,7 +20,7 @@ digitalWrite(inWaitingPin, LOW);
 
 // main loop
 void loop(){
-  Recursion(redValue, greenValue, blueValue, currentPin, nextPin, inWaitingPin); 
+  Recursion(redValue, greenValue, blueValue, currentPin, nextPin, inWaitingPin); // calls the recursion algorithim and passes values. 
   
 }
  void Recursion(int current, int next, int inWaiting, int currentPin, int nextPin, int inWaitingPin)
@@ -34,10 +34,11 @@ void loop(){
   {
   current -= 1;
   next += 1;
-  analogWrite(currentPin, current);
+  analogWrite(currentPin, current); // sends the color values and pin number
   analogWrite(nextPin, next);
   delay(delayTime);
 }
-   Recursion(next, inWaiting, current, nextPin, inWaitingPin, currentPin); // recursive call switching inputs
+   Recursion(next, inWaiting, current, nextPin, inWaitingPin, currentPin); // recursive call switching inputs after current fades out and
+                                                                           // and next comes in. 
 }
 
